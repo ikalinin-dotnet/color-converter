0f0d4e83a343eefbc4ffcda55f322ea480757af9
Illia Kalinin <ikalinin911@gmail.com>
2026-05-16 12:27:28 +0200

    refactor: slim controller to use new service layer

--- Covide/Covide.Web/Startup.cs
+++ Covide/Covide.Web/Startup.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Covide.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Covide.Web
 {
@@ -27,6 +21,9 @@ namespace Covide.Web
             services.AddControllers();
 
             services.AddEntityFrameworkSqlite().AddDbContext<CovideDataContext>();
+
+            services.AddScoped<IColorNameService, ColorNameService>();
+            services.AddScoped<ColorConversionService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
